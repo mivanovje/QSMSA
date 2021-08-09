7d9087fe4403267570893854282cb8075fd54a05
Max Ivanov <mivanovje@gmail.com>
2021-08-10 00:18:08 +0300

    Add AOP  logging in_out parameters and exceptions

--- src/main/java/mgivanov/qsmsa/rest/GreetingController.java
+++ src/main/java/mgivanov/qsmsa/rest/GreetingController.java
@@ -1,20 +1,37 @@
 package mgivanov.qsmsa.rest;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import mgivanov.qsmsa.entity.Greeting;
+import mgivanov.qsmsa.model.Greeting;
+import mgivanov.qsmsa.util.annotation.LogInOutParameters;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
+    @LogInOutParameters
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        int a = (int) ( Math.random() * 2 );
+        if(a==1){
+            throw new RuntimeException();
+        }
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/count")
+    @LogInOutParameters
+    public AtomicLong getCounter() {
+        return counter;
+    }
+
+
 }
---
+++ src/main/java/mgivanov/qsmsa/util/aspect/GreetingControllerLoggingAspect.java
@@ -0,0 +1,54 @@
+package mgivanov.qsmsa.util.aspect;
+
+import mgivanov.qsmsa.model.Greeting;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class GreetingControllerLoggingAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("@annotation(mgivanov.qsmsa.util.annotation.LogInOutParameters)")
+    public void callAtMyServicePublic() {
+    }
+
+    @Before("callAtMyServicePublic()")
+    public void beforeCallAtMethod1(JoinPoint jp) {
+        String args = Arrays.stream(jp.getArgs())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        logger.info("Массив входящих параметров метода - " + jp.getSignature().getName() + ", args=[" + args + "]");
+    }
+
+    @AfterReturning(value = "callAtMyServicePublic()", returning = "returnValue")
+    public void afterCallAt(JoinPoint jp, Object returnValue) {
+        if (returnValue != null) {
+            System.out.printf("Успешно выполнен метод - %s, класса- %s, с результатом выполнения - %s\n",
+                    jp.getSignature().getName(),
+                    jp.getSourceLocation().getWithinType().getName(),
+                    returnValue);
+        } else {
+            System.out.printf("Успешно выполнен метод - %s, класса- %s\n",
+                    jp.getSignature().getName(),
+                    jp.getSourceLocation().getWithinType().getName());
+        }
+    }
+
+    @AfterThrowing(value = "callAtMyServicePublic()", throwing = "exception")
+    public void recordFailedExecution(JoinPoint joinPoint, Exception exception) {
+        System.out.printf("Метод - %s, класса- %s, был аварийно завершен с исключением - %s\n",
+                joinPoint.getSignature().getName(),
+                joinPoint.getSourceLocation().getWithinType().getName(),
+                exception);
+    }
+
+
+}
