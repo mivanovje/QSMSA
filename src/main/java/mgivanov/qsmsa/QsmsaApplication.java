2d4cd33af5ff1901e4b48057b1c97843d3bc8d8b
Max Ivanov <mivanovje@gmail.com>
2021-08-09 13:16:30 +0300

    Initial commit

---
+++ src/main/java/mgivanov/qsmsa/QsmsaApplication.java
@@ -0,0 +1,13 @@
+package mgivanov.qsmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsmsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsmsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mgivanov/qsmsa/QsmsaApplicationTests.java
@@ -0,0 +1,13 @@
+package mgivanov.qsmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QsmsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
